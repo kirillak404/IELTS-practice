fd4e5122e19dbac5e3a86a9f1cff9611a1e85dbd
Kirill <kirillak404@gmail.com>
2023-10-04 14:36:34 +0200

    build: app testing 1

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
-from app import create_app
+# from app import create_app
+#
+# app = create_app()
+#
+# if __name__ == '__main__':
+#     app.run()
 
-app = create_app()
 
-if __name__ == '__main__':
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+if __name__ == "__main__":
     app.run()
